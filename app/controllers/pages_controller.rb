18e79a307a64cbdaaa9b5181b95eb05309bfd421
Nguyen Tri Minh <minh.nguyentri.95@gmail.com>
2017-10-10 13:41:12 +0700

    fix shoe index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     unless user_signed_in?
       redirect_to new_user_session_path
+    else
+      redirect_to shoes_path
     end
   end
 end
