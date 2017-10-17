057306d3fa8fddca336af05a48a37922f5e63502
linhdang <dangduylinh.bk@gmail.com>
2017-10-17 08:13:32 +0700

    fix user profile and m-a-n-y-t-h-i-n-g

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
   def gravatar_for user, options = { size: 50 }
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar")
   end
 end
