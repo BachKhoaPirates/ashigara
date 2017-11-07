948c5327ccf89f7b4a7856f1f67f3431d0ae54cd
Tran B.V. Son <tranbavinhson@gmail.com>
2017-11-07 10:07:31 +0700

    recommend

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -8,6 +8,8 @@ class ShoesController < ApplicationController
     @shoe = Shoe.find_by id: params[:id]
     @reviews = @shoe.reviews
     @shoe_shops = @shoe.shoe_shops.includes(:shop)
+    Rails.logger.info "Shoe Brand: #{@shoe.brand_id}"
+    @recommend_shoe = Shoe.where(brand_id: @shoe.brand_id).limit(5)
     unless @shoe.present?
       redirect_to root_path
     end
