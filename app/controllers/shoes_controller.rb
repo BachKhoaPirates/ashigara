33ecaf9029a7f50b69601db04e3b2a0962e462b5
Tran B.V. Son <tranbavinhson@gmail.com>
2017-11-21 13:44:43 +0700

    fix dummy bug

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -9,28 +9,31 @@ class ShoesController < ApplicationController
     @reviews = @shoe.reviews
     @shoe_shops = @shoe.shoe_shops.includes(:shop)
 
-    if current_user
-      @fitted_size = Size.where(foot_length: current_user.foot_length,
-        foot_width: current_user.foot_width, shoe_id: @shoe.id)
-      if @fitted_size == nil || @fitted_size.count == 0
-        @fitted_size = Size.where(shoe_id: @shoe.id).first
-      else
-        @fitted_size = @fitted_size.first
+    if user_signed_in?
+      if current_user
+        @fitted_size = Size.where(foot_length: current_user.foot_length,
+          foot_width: current_user.foot_width, shoe_id: @shoe.id)
+        if @fitted_size == nil || @fitted_size.count == 0
+          @fitted_size = Size.where(shoe_id: @shoe.id).first
+        else
+          @fitted_size = @fitted_size.first
+        end
+      end
+
+      if !params[:foot_width].blank? && !params[:foot_length].blank?
+        Rails.logger.info "Length: #{params[:foot_length]} - Width: #{params[:foot_width]}"
+        @searched_fit_size = Size.where(foot_length: params[:foot_length],
+          foot_width: params[:foot_width], shoe_id: @shoe.id)
+        if @searched_fit_size == nil || @searched_fit_size.count == 0
+          @searched_fit_size = Size.where(shoe_id: @shoe.id).first
+        else
+          @searched_fit_size = @searched_fit_size.first
+        end
+        flash[:notice] = 'Suggest shoe size: ' + @searched_fit_size.size.to_s
+      end
       end
     end
 
-    if !params[:foot_width].blank? && !params[:foot_length].blank?
-      Rails.logger.info "Length: #{params[:foot_length]} - Width: #{params[:foot_width]}"
-      @searched_fit_size = Size.where(foot_length: params[:foot_length],
-        foot_width: params[:foot_width], shoe_id: @shoe.id)
-      if @searched_fit_size == nil || @searched_fit_size.count == 0
-        @searched_fit_size = Size.where(shoe_id: @shoe.id).first
-      else
-        @searched_fit_size = @searched_fit_size.first
-      end
-      flash[:notice] = 'Suggest shoe size: ' + @searched_fit_size.size.to_s
-    end
-  end
 
   def autocomplete
     render json: Shoe.search(params[:query], {
